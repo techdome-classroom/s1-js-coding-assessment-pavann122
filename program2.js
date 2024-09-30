a20863b1e2966a424324f12de56cb0c3aa5b09ba
Pavan Kumar Vishwakarma <prabhashdwivedi725@gmail.com>
2024-09-30 12:45:59 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -29,8 +29,6 @@ var romanToInt = function(s) {
     
     return total;
 }
-
-// Example Usage
 console.log(romanToInt("III")); // Output: 3
 console.log(romanToInt("LVIII")); // Output: 58
 console.log(romanToInt("MCMXCIV")); // Output: 1994
