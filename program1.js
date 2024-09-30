b8ecffc017fef7cdac532983fee4edb5b5434f76
Pavan Kumar Vishwakarma <prabhashdwivedi725@gmail.com>
2024-09-30 12:24:21 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,7 +11,7 @@ var isValid = function(s) {
         ']':'['
     };
     for(let char of s){
-        if (char in bracketMap)
+        if (char in bracketMap){
             const topElement=stack.length ? stack.pop() :'#';
             if(bracketMap[char] !==topElement){
                 return false;
